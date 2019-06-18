f95c71f24ebff270ed0c00b481099f9c9fe12293
maxchehab <maxchehab@gmail.com>
2019-06-17 21:00:55 -0400

    refactor: workos.spect.ts

--- src/workos.spec.ts
+++ src/workos.spec.ts
@@ -12,9 +12,9 @@ import {
 const mock = new MockAdapater(axios);
 const event = {
   group: 'WorkOS',
-  actor_name: 'WorkOS@workos.com',
+  actor_name: 'WorkOS@example.com',
   actor_id: 'user_1',
-  location: '208.185.185.131',
+  location: ' 192.0.0.8',
   occurred_at: new Date(0),
   target_name: 'Security Audit 2018',
   target_id: 'document_39127',
@@ -24,76 +24,86 @@ const event = {
 
 describe('WorkOS', () => {
   describe('createEvent', () => {
-    it('posts Event successfuly', async () => {
-      mock.onPost().reply(201, { success: true });
+    describe('when the api responds with a 201 CREATED', () => {
+      it('posts Event successfuly', async () => {
+        mock.onPost().reply(201, { success: true });
 
-      const workos = new WorkOS({
-        apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
+        const workos = new WorkOS({
+          apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
+        });
+
+        await expect(workos.createEvent(event)).resolves.toBeUndefined();
       });
-
-      await expect(workos.createEvent(event)).resolves.toBeUndefined();
     });
 
-    it('throws an UnauthorizedException when the api returns a 401', async () => {
-      mock.onPost().reply(401, { message: 'Unauthorized' });
+    describe('when the api responds with a 401', () => {
+      it('throws an UnauthorizedException', async () => {
+        mock.onPost().reply(401, { message: 'Unauthorized' });
 
-      const workos = new WorkOS({ apiKey: 'invlaid apikey' });
+        const workos = new WorkOS({ apiKey: 'invlaid apikey' });
 
-      await expect(workos.createEvent(event)).rejects.toStrictEqual(
-        new UnauthorizedException(),
-      );
+        await expect(workos.createEvent(event)).rejects.toStrictEqual(
+          new UnauthorizedException(),
+        );
+      });
     });
 
-    it('throws an UnprocessableEntity when the api returns a 422', async () => {
-      const errors = [
-        {
-          field: 'target_id',
-          code: 'target_id must be a string',
-        },
-        {
-          field: 'occurred_at',
-          code: 'occurred_at must be a ISOString',
-        },
-      ];
+    describe('when the api responds with a 422', () => {
+      it('throws an UnprocessableEntity', async () => {
+        const errors = [
+          {
+            field: 'target_id',
+            code: 'target_id must be a string',
+          },
+          {
+            field: 'occurred_at',
+            code: 'occurred_at must be a ISOString',
+          },
+        ];
 
-      mock.onPost().reply(422, {
-        message: 'Validation failed',
-        errors,
+        mock.onPost().reply(422, {
+          message: 'Validation failed',
+          errors,
+        });
+
+        const workos = new WorkOS({
+          apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
+        });
+
+        await expect(workos.createEvent(event)).rejects.toStrictEqual(
+          new UnprocessableEntityException(errors),
+        );
       });
-
-      const workos = new WorkOS({
-        apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
-      });
-
-      await expect(workos.createEvent(event)).rejects.toStrictEqual(
-        new UnprocessableEntityException(errors),
-      );
     });
   });
 
   describe('post', () => {
-    it('throws an NotFoundException when the api returns a 404', async () => {
-      mock.onPost().reply(404, { message: 'Not Found' });
+    describe('when the api responds with a 404', () => {
+      it('throws a NotFoundException', async () => {
+        mock.onPost().reply(404, { message: 'Not Found' });
 
-      const workos = new WorkOS({
-        apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
+        const workos = new WorkOS({
+          apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
+        });
+
+        await expect(workos.post({}, '/path')).rejects.toStrictEqual(
+          new NotFoundException('/path'),
+        );
       });
-
-      await expect(workos.post({}, '/path')).rejects.toStrictEqual(
-        new NotFoundException('/path'),
-      );
     });
 
-    it('throws an InternalServerErrorException when the api returns a 500', async () => {
-      mock.onPost().reply(500);
+    describe('when the api responds with a 500', () => {
+      it('throws an InternalServerErrorException', async () => {
+        mock.onPost().reply(500);
 
-      const workos = new WorkOS({
-        apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
+        const workos = new WorkOS({
+          apiKey: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
+        });
+
+        await expect(workos.post({}, '/path')).rejects.toStrictEqual(
+          new InternalServerErrorException(),
+        );
       });
-
-      await expect(workos.post({}, '/path')).rejects.toStrictEqual(
-        new InternalServerErrorException(),
-      );
     });
   });
 });
