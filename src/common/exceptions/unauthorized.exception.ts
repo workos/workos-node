a11a98302fc49d91c43f0501c4901cc5b6e1da6a
Max Chehab <maxchehab@users.noreply.github.com>
2019-06-20 18:32:39 -0400

    Update src/common/exceptions/unauthorized.exception.ts
    Co-Authored-By: Mark Tran <mark.tran@gmail.com>

--- src/common/exceptions/unauthorized.exception.ts
+++ src/common/exceptions/unauthorized.exception.ts
@@ -4,5 +4,5 @@ export class UnauthorizedException implements HttpException {
   readonly status: number = 401;
   readonly name: string = 'UnauthorizedException';
   readonly message: string =
-    'Could not authorize the request. Maybe your api key is invalid?';
+    'Could not authorize the request. Maybe your API key is invalid?';
 }
