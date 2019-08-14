d7d7e0965488f9767c4e8fe02aa00106fb844c09
Max Chehab <maxchehab@users.noreply.github.com>
2019-08-13 23:25:29 -0400

    Fix/append unprocessable error messages (#20)
    * fix: append unprocessable errors to exception message
    
    * fix tests to use snapshot
    * add snapshot
    
    * fix: update snap for date error
    
    * fix: exception typo

--- src/common/exceptions/no-api-key-provided.exception.ts
+++ src/common/exceptions/no-api-key-provided.exception.ts
@@ -2,6 +2,6 @@ export class NoApiKeyProvidedException extends Error {
   readonly status: number = 500;
   readonly name: string = 'NoApiKeyProvidedException';
   readonly message: string =
-    `Missing API key. Pass it to the constructor (new WorkoS({ key: "sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU" })) ` +
+    `Missing API key. Pass it to the constructor (new WorkOS({ key: "sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU" })) ` +
     `or define it in the WORKOS_API_KEY environment variable.`;
 }
