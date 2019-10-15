9ad882482acb3a6bcb90aa323bfba86089ac1ed8
maxchehab <maxchehab@gmail.com>
2019-10-15 20:00:11 +0000

    update tests

--- src/workos.spec.ts
+++ src/workos.spec.ts
@@ -3,24 +3,12 @@ import MockAdapater from 'axios-mock-adapter';
 
 import WorkOS from './workos';
 import {
-  NotFoundException,
   InternalServerErrorException,
-  UnauthorizedException,
   NoApiKeyProvidedException,
+  NotFoundException,
 } from './common/exceptions';
 
 const mock = new MockAdapater(axios);
-const event = {
-  group: 'WorkOS',
-  actor_name: 'WorkOS@example.com',
-  actor_id: 'user_1',
-  location: ' 192.0.0.8',
-  occurred_at: new Date(0),
-  target_name: 'Security Audit 2018',
-  target_id: 'document_39127',
-  action: 'did.a.thing',
-  action_type: 'U',
-};
 
 describe('WorkOS', () => {
   describe('constructor', () => {
@@ -53,60 +41,8 @@ describe('WorkOS', () => {
     describe('when api key is provided with constructor', () => {
       it('constructs successfuly', async () => {
         expect(
-          () => new WorkOS({ key: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU' }),
+          () => new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU'),
         ).not.toThrow();
-      });
-    });
-  });
-
-  describe('createEvent', () => {
-    describe('when the api responds with a 201 CREATED', () => {
-      it('posts Event successfuly', async () => {
-        mock.onPost().reply(201, { success: true });
-
-        const workos = new WorkOS({
-          key: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
-        });
-
-        await expect(workos.createEvent(event)).resolves.toBeUndefined();
-      });
-    });
-
-    describe('when the api responds with a 401', () => {
-      it('throws an UnauthorizedException', async () => {
-        mock.onPost().reply(401, { message: 'Unauthorized' });
-
-        const workos = new WorkOS({ key: 'invalid apikey' });
-
-        await expect(workos.createEvent(event)).rejects.toStrictEqual(
-          new UnauthorizedException(),
-        );
-      });
-    });
-
-    describe('when the api responds with a 422', () => {
-      it('throws an UnprocessableEntity', async () => {
-        const errors = [
-          {
-            field: 'target_id',
-            code: 'target_id must be a string',
-          },
-          {
-            field: 'occurred_at',
-            code: 'occurred_at must be an ISO 8601 date string',
-          },
-        ];
-
-        mock.onPost().reply(422, {
-          message: 'Validation failed',
-          errors,
-        });
-
-        const workos = new WorkOS({
-          key: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
-        });
-
-        await expect(workos.createEvent(event)).rejects.toMatchSnapshot();
       });
     });
   });
@@ -116,9 +52,7 @@ describe('WorkOS', () => {
       it('throws a NotFoundException', async () => {
         mock.onPost().reply(404, { message: 'Not Found' });
 
-        const workos = new WorkOS({
-          key: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
-        });
+        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
 
         await expect(workos.post({}, '/path')).rejects.toStrictEqual(
           new NotFoundException('/path'),
@@ -130,9 +64,7 @@ describe('WorkOS', () => {
       it('throws an InternalServerErrorException', async () => {
         mock.onPost().reply(500);
 
-        const workos = new WorkOS({
-          key: 'sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU',
-        });
+        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
 
         await expect(workos.post({}, '/path')).rejects.toStrictEqual(
           new InternalServerErrorException(),
