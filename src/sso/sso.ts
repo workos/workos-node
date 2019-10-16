fbd3904c0ce3799238d60515e4be92f7992f7f37
maxchehab <maxchehab@gmail.com>
2019-10-16 19:36:44 +0000

    add: getAuthorzeURL function

--- src/sso/sso.ts
+++ src/sso/sso.ts
@@ -1,5 +1,27 @@
+import queryString from 'query-string';
+
+import { API_HOSTNAME } from '../common/constants';
+import { AuthorizeURLOptions } from './interfaces/authorize-url-options.interface';
 import WorkOS from '../workos';
 
 export class SSO {
   constructor(private readonly workos: WorkOS) {}
+
+  getAuthorizeURL({
+    domain,
+    projectID,
+    redirectURI,
+    state,
+  }: AuthorizeURLOptions): string {
+    const { apiHostname = API_HOSTNAME } = this.workos.options;
+
+    const query = queryString.stringify({
+      domain,
+      client_id: projectID,
+      redirect_uri: redirectURI,
+      state,
+    });
+
+    return `https://${apiHostname}/sso/authorize?${query}`;
+  }
 }
