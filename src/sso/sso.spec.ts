de9475ebfd16908b26fd719336e677451c524487
Max Chehab <maxchehab@users.noreply.github.com>
2019-10-17 09:44:45 -0700

    Maxchehab/ch768/add authorizeurl function to sso module (#29)
    * add: query string as a dependency
    
    * fix: typo
    
    * create: sso module
    
    * add: getAuthorzeURL function
    
    * add: tests for authorize URL function
    
    * getAuthorizeURL -> authorizeURL
    
    * update tests

---
+++ src/sso/sso.spec.ts
@@ -0,0 +1,50 @@
+import WorkOS from '../workos';
+
+describe('SSO', () => {
+  describe('getAuthorizationURL', () => {
+    describe('with no custom api hostname', () => {
+      it('generates an authorize url with the default api hostname', () => {
+        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
+
+        const url = workos.sso.getAuthorizationURL({
+          domain: 'lyft.com',
+          projectID: 'proj_123',
+          redirectURI: 'example.com/sso/workos/callback',
+        });
+
+        expect(url).toMatchSnapshot();
+      });
+    });
+
+    describe('with a custom api hostname', () => {
+      it('generates an authorize url with the custom api hostname', () => {
+        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU', {
+          apiHostname: 'api.workos.dev',
+        });
+
+        const url = workos.sso.getAuthorizationURL({
+          domain: 'lyft.com',
+          projectID: 'proj_123',
+          redirectURI: 'example.com/sso/workos/callback',
+        });
+
+        expect(url).toMatchSnapshot();
+      });
+    });
+
+    describe('with state', () => {
+      it('generates an authorize url the provided state', () => {
+        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
+
+        const url = workos.sso.getAuthorizationURL({
+          domain: 'lyft.com',
+          projectID: 'proj_123',
+          redirectURI: 'example.com/sso/workos/callback',
+          state: 'custom state',
+        });
+
+        expect(url).toMatchSnapshot();
+      });
+    });
+  });
+});
