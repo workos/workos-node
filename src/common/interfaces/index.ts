de44c28f7554d744ab9e3b7fd638b80f568e37f2
maxchehab <maxchehab@gmail.com>
2020-01-02 19:26:59 +0000

    add requestID as a property to the HttpException

--- src/common/interfaces/index.ts
+++ src/common/interfaces/index.ts
@@ -1,4 +1,4 @@
-export { WorkOSOptions } from './workos-options.interface';
 export { Event } from './event.interface';
 export { HttpException } from './http-exception.interface';
-export { UnprocessableEntityError } from './unprocessable-entity-error';
+export { UnprocessableEntityError } from './unprocessable-entity-error.interface';
+export { WorkOSOptions } from './workos-options.interface';
