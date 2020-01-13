63db12ebe1dd13aafb6dc2aa112c7eba4a93b635
maxchehab <maxchehab@gmail.com>
2020-01-13 23:00:22 +0000

    add idempotency header if provided

--- src/audit-log/audit-log.ts
+++ src/audit-log/audit-log.ts
@@ -1,10 +1,17 @@
+import { CreateEventOptions } from './interfaces/create-event-options.interface';
 import { Event } from '../common/interfaces';
 import WorkOS from '../workos';
 
 export class AuditLog {
   constructor(private readonly workos: WorkOS) {}
 
-  async createEvent(event: Event) {
-    await this.workos.post('/events', event);
+  async createEvent(event: Event, { idempotencyKey }: CreateEventOptions = {}) {
+    const headers: any = {};
+
+    if (idempotencyKey) {
+      headers['Idempotency-Key'] = idempotencyKey;
+    }
+
+    await this.workos.post('/events', event, { headers });
   }
 }
