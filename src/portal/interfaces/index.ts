15b9dafb06044ac3fd30e82ee3ed26e39b4f6ade
Willman Duffy <willman@workos.com>
2020-09-11 12:11:41 -0400

    Add generateLink to Portal
    * Adds an SDK method to generate an Admin Portal link.

---
+++ src/portal/interfaces/index.ts
@@ -0,0 +1,4 @@
+export { GeneratePortalLinkIntent } from './generate-portal-link-intent.interface';
+export { ListOrganizationsOptions } from './list-organizations-options.interface';
+export { OrganizationDomain } from './organization-domain.interface';
+export { Organization } from './organization.interface';
