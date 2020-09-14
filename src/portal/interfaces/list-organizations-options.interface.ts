1c63d424e30cebaefb45531d61f30c29fadf4e2b
Willman Duffy <willman@workos.com>
2020-09-14 15:14:22 -0400

    Rename listOrganizations arg "domain" to "domains"
    * Modifies the arg to accept an array of strings, instead of a
      string.

--- src/portal/interfaces/list-organizations-options.interface.ts
+++ src/portal/interfaces/list-organizations-options.interface.ts
@@ -1,5 +1,5 @@
 import { PaginationOptions } from '../../common/interfaces/pagination-options.interface';
 
 export interface ListOrganizationsOptions extends PaginationOptions {
-  domain?: string;
+  domains?: string[];
 }
