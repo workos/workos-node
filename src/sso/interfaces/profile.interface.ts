255366c02775a1c54caaf8382c9d3c563e036dfb
Taylor Hodge <j.taylor.hodge@gmail.com>
2020-10-14 17:36:47 -0400

    Expose `connection_id` on Profile Objects (#241)

--- src/sso/interfaces/profile.interface.ts
+++ src/sso/interfaces/profile.interface.ts
@@ -3,6 +3,7 @@ import { ConnectionType } from './connection-type.enum';
 export interface Profile {
   id: string;
   idp_id: string;
+  connection_id: string;
   connection_type: ConnectionType;
   email: string;
   first_name?: string;
