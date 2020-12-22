e25025f8b222b08bad499233c49e3ce0cfd25854
loferris <lo.ferris@workos.com>
2020-12-22 13:03:46 -0800

    made test cases reflect clientID, removed projectID from tests

--- src/sso/sso.spec.ts
+++ src/sso/sso.spec.ts
@@ -6,7 +6,7 @@ import { WorkOS } from '../workos';
 describe('SSO', () => {
   describe('SSO', () => {
     describe('getAuthorizationURL', () => {
-      describe('with no custom api hostname and clientID', () => {
+      describe('with no custom api hostname', () => {
         it('generates an authorize url with the default api hostname', () => {
           const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
 
@@ -20,21 +20,7 @@ describe('SSO', () => {
         });
       });
 
-      describe('with no custom api hostname and projectID', () => {
-        it('generates an authorize url with the default api hostname', () => {
-          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-
-          const url = workos.sso.getAuthorizationURL({
-            domain: 'lyft.com',
-            projectID: 'proj_123',
-            redirectURI: 'example.com/sso/workos/callback',
-          });
-
-          expect(url).toMatchSnapshot();
-        });
-      });
-
-      describe('with no domain or provider and clientID', () => {
+      describe('with no domain or provider', () => {
         it('throws an error for incomplete arguments', () => {
           const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
 
@@ -48,21 +34,7 @@ describe('SSO', () => {
         });
       });
 
-      describe('with no domain or provider and projectID', () => {
-        it('throws an error for incomplete arguments', () => {
-          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-
-          const urlFn = () =>
-            workos.sso.getAuthorizationURL({
-              projectID: 'proj_123',
-              redirectURI: 'example.com/sso/workos/callback',
-            });
-
-          expect(urlFn).toThrowErrorMatchingSnapshot();
-        });
-      });
-
-      describe('with a provider and clientID', () => {
+      describe('with a provider', () => {
         it('generates an authorize url with the provider', () => {
           const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU', {
             apiHostname: 'api.workos.dev',
@@ -78,23 +50,7 @@ describe('SSO', () => {
         });
       });
 
-      describe('with a provider and projectID', () => {
-        it('generates an authorize url with the provider', () => {
-          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU', {
-            apiHostname: 'api.workos.dev',
-          });
-
-          const url = workos.sso.getAuthorizationURL({
-            provider: 'Google',
-            projectID: 'proj_123',
-            redirectURI: 'example.com/sso/workos/callback',
-          });
-
-          expect(url).toMatchSnapshot();
-        });
-      });
-
-      describe('with a custom api hostname and clientID', () => {
+      describe('with a custom api hostname', () => {
         it('generates an authorize url with the custom api hostname', () => {
           const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU', {
             apiHostname: 'api.workos.dev',
@@ -110,23 +66,7 @@ describe('SSO', () => {
         });
       });
 
-      describe('with a custom api hostname and projectID', () => {
-        it('generates an authorize url with the custom api hostname', () => {
-          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU', {
-            apiHostname: 'api.workos.dev',
-          });
-
-          const url = workos.sso.getAuthorizationURL({
-            domain: 'lyft.com',
-            projectID: 'proj_123',
-            redirectURI: 'example.com/sso/workos/callback',
-          });
-
-          expect(url).toMatchSnapshot();
-        });
-      });
-
-      describe('with state and clientID', () => {
+      describe('with state', () => {
         it('generates an authorize url with the provided state', () => {
           const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
 
@@ -142,184 +82,132 @@ describe('SSO', () => {
       });
     });
 
-    describe('with state and projectID', () => {
-      it('generates an authorize url with the provided state', () => {
-        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-
-        const url = workos.sso.getAuthorizationURL({
-          domain: 'lyft.com',
-          projectID: 'proj_123',
-          redirectURI: 'example.com/sso/workos/callback',
-          state: 'custom state',
-        });
-
-        expect(url).toMatchSnapshot();
-      });
-    });
-  });
-
-  describe('getProfile and clientID', () => {
-    describe('with all information provided', () => {
-      it('sends a request to the WorkOS api for a profile', async () => {
-        const mock = new MockAdapter(axios);
-        mock.onPost('/sso/token').reply(200, {
-          profile: {
-            id: 'prof_123',
-            idp_id: '123',
-            connection_id: 'conn_123',
-            connection_type: 'OktaSAML',
-            email: 'foo@test.com',
-            first_name: 'foo',
-            last_name: 'bar',
-            raw_attributes: {
+    describe('getProfile', () => {
+      describe('with all information provided', () => {
+        it('sends a request to the WorkOS api for a profile', async () => {
+          const mock = new MockAdapter(axios);
+          mock.onPost('/sso/token').reply(200, {
+            profile: {
+              id: 'prof_123',
+              idp_id: '123',
+              connection_id: 'conn_123',
+              connection_type: 'OktaSAML',
               email: 'foo@test.com',
               first_name: 'foo',
               last_name: 'bar',
+              raw_attributes: {
+                email: 'foo@test.com',
+                first_name: 'foo',
+                last_name: 'bar',
+              },
             },
-          },
+          });
+
+          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
+          const profile = await workos.sso.getProfile({
+            code: 'authorization_code',
+            clientID: 'client_123',
+          });
+
+          expect(mock.history.post.length).toBe(1);
+          const { data, headers } = mock.history.post[0];
+
+          expect(data).toMatchSnapshot();
+          expect(headers).toMatchSnapshot();
+          expect(profile).toMatchSnapshot();
         });
-
-        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-        const profile = await workos.sso.getProfile({
-          code: 'authorization_code',
-          clientID: 'client_123',
-        });
-
-        expect(mock.history.post.length).toBe(1);
-        const { data, headers } = mock.history.post[0];
-
-        expect(data).toMatchSnapshot();
-        expect(headers).toMatchSnapshot();
-        expect(profile).toMatchSnapshot();
-      });
-    });
-  });
-
-  describe('getProfile and projectID', () => {
-    describe('with all information provided', () => {
-      it('sends a request to the WorkOS api for a profile', async () => {
-        const mock = new MockAdapter(axios);
-        mock.onPost('/sso/token').reply(200, {
-          profile: {
-            id: 'prof_123',
-            idp_id: '123',
-            connection_id: 'conn_123',
-            connection_type: 'OktaSAML',
-            email: 'foo@test.com',
-            first_name: 'foo',
-            last_name: 'bar',
-            raw_attributes: {
-              email: 'foo@test.com',
-              first_name: 'foo',
-              last_name: 'bar',
-            },
-          },
-        });
-
-        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-        const profile = await workos.sso.getProfile({
-          code: 'authorization_code',
-          projectID: 'proj_123',
-        });
-
-        expect(mock.history.post.length).toBe(1);
-        const { data, headers } = mock.history.post[0];
-
-        expect(data).toMatchSnapshot();
-        expect(headers).toMatchSnapshot();
-        expect(profile).toMatchSnapshot();
-      });
-    });
-  });
-
-  describe('promoteDraftConnection', () => {
-    describe('with valid token', () => {
-      it('responds with a 201 CREATED status', async () => {
-        const token = 'wOrkOStoKeN';
-        const endpoint = `/draft_connections/${token}/activate`;
-
-        const mock = new MockAdapter(axios);
-        mock.onPost(endpoint).reply(201);
-
-        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-        await workos.sso.promoteDraftConnection({ token });
-
-        expect(mock.history.post.length).toBe(1);
-        const post = mock.history.post[0];
-        expect(post.url).toEqual(endpoint);
       });
     });
 
-    describe('with invalid token', () => {
-      it('responds with a 404 NOT FOUND status', async () => {
-        const token = 'wOrkOStoKeN';
-        const endpoint = `/draft_connections/${token}/activate`;
+    describe('promoteDraftConnection', () => {
+      describe('with valid token', () => {
+        it('responds with a 201 CREATED status', async () => {
+          const token = 'wOrkOStoKeN';
+          const endpoint = `/draft_connections/${token}/activate`;
 
-        const mock = new MockAdapter(axios);
-        mock
-          .onPost(endpoint)
-          .reply(
-            404,
-            { message: 'Bad Request' },
-            { 'X-Request-ID': 'a-request-id' },
-          );
+          const mock = new MockAdapter(axios);
+          mock.onPost(endpoint).reply(201);
 
-        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-
-        try {
+          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
           await workos.sso.promoteDraftConnection({ token });
-          throw 'should not be called';
-        } catch (err) {
+
+          expect(mock.history.post.length).toBe(1);
           const post = mock.history.post[0];
           expect(post.url).toEqual(endpoint);
-          expect(err.status).toEqual(404);
-        }
+        });
+      });
+
+      describe('with invalid token', () => {
+        it('responds with a 404 NOT FOUND status', async () => {
+          const token = 'wOrkOStoKeN';
+          const endpoint = `/draft_connections/${token}/activate`;
+
+          const mock = new MockAdapter(axios);
+          mock
+            .onPost(endpoint)
+            .reply(
+              404,
+              { message: 'Bad Request' },
+              { 'X-Request-ID': 'a-request-id' },
+            );
+
+          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
+
+          try {
+            await workos.sso.promoteDraftConnection({ token });
+            throw 'should not be called';
+          } catch (err) {
+            const post = mock.history.post[0];
+            expect(post.url).toEqual(endpoint);
+            expect(err.status).toEqual(404);
+          }
+        });
       });
     });
-  });
 
-  describe('createConnection', () => {
-    describe('with valid token', () => {
-      it('responds with a 201 CREATED status', async () => {
-        const source = 'wOrkOStoKeN';
+    describe('createConnection', () => {
+      describe('with valid token', () => {
+        it('responds with a 201 CREATED status', async () => {
+          const source = 'wOrkOStoKeN';
 
-        const mock = new MockAdapter(axios);
-        mock.onPost('/connections').reply(201);
+          const mock = new MockAdapter(axios);
+          mock.onPost('/connections').reply(201);
 
-        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-        await workos.sso.createConnection({ source });
-
-        expect(mock.history.post.length).toBe(1);
-        const post = mock.history.post[0];
-        expect(post.url).toEqual('/connections');
-      });
-    });
-
-    describe('with invalid token', () => {
-      it('responds with a 404 NOT FOUND status', async () => {
-        const source = 'wOrkOStoKeN';
-
-        const mock = new MockAdapter(axios);
-        mock.onPost('/connections').reply(
-          404,
-          {
-            message: 'Bad Request',
-          },
-          {
-            'X-Request-ID': 'a-request-id',
-          },
-        );
-
-        const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
-
-        try {
+          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
           await workos.sso.createConnection({ source });
-          throw 'should not be called';
-        } catch (err) {
+
+          expect(mock.history.post.length).toBe(1);
           const post = mock.history.post[0];
           expect(post.url).toEqual('/connections');
-          expect(err.status).toEqual(404);
-        }
+        });
+      });
+
+      describe('with invalid token', () => {
+        it('responds with a 404 NOT FOUND status', async () => {
+          const source = 'wOrkOStoKeN';
+
+          const mock = new MockAdapter(axios);
+          mock.onPost('/connections').reply(
+            404,
+            {
+              message: 'Bad Request',
+            },
+            {
+              'X-Request-ID': 'a-request-id',
+            },
+          );
+
+          const workos = new WorkOS('sk_test_Sz3IQjepeSWaI4cMS4ms4sMuU');
+
+          try {
+            await workos.sso.createConnection({ source });
+            throw 'should not be called';
+          } catch (err) {
+            const post = mock.history.post[0];
+            expect(post.url).toEqual('/connections');
+            expect(err.status).toEqual(404);
+          }
+        });
       });
     });
   });
