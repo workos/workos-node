979892102bd5cc1e322a1b3b0e4349d66f12de19
Taylor Hodge <j.taylor.hodge@gmail.com>
2021-02-11 15:15:45 -0500

    update connection types

--- src/sso/interfaces/connection-type.enum.ts
+++ src/sso/interfaces/connection-type.enum.ts
@@ -1,4 +1,14 @@
 export enum ConnectionType {
+  ADFSSAML = 'ADFSSAML',
   AzureSAML = 'AzureSAML',
+  GenericOIDC = 'GenericOIDC',
+  GenericSAML = 'GenericSAML',
+  GoogleOAuth = 'GoogleOAuth',
+  GoogleSAML = 'GoogleSAML',
+  MagicLink = 'MagicLink',
   OktaSAML = 'OktaSAML',
+  OneLoginSAML = 'OneLoginSAML',
+  PingFederateSAML = 'PingFederateSAML',
+  PingOneSAML = 'PingOneSAML',
+  VMwareSAML = 'VMwareSAML',
 }
