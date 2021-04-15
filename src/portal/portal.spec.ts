1499114225337803b9aeeaeb17ada33e1a7b9849
Marshall Bowers <elliott.codes@gmail.com>
2021-04-15 16:24:11 -0400

    Fix data returned from `generateLink`

--- src/portal/portal.spec.ts
+++ src/portal/portal.spec.ts
@@ -58,13 +58,13 @@ describe('Portal', () => {
         it('returns an Admin Portal link', async () => {
           mock.onPost().reply(201, generateLink);
 
-          const subject = await workos.portal.generateLink({
+          const adminPortalLink = await workos.portal.generateLink({
             intent: GeneratePortalLinkIntent.SSO,
             organization: 'org_01EHQMYV6MBK39QC5PZXHY59C3',
             returnUrl: 'https://www.example.com',
           });
 
-          expect(subject.link).toEqual(
+          expect(adminPortalLink).toEqual(
             'https://id.workos.com/portal/launch?secret=secret',
           );
         });
@@ -74,13 +74,13 @@ describe('Portal', () => {
         it('returns an Admin Portal link', async () => {
           mock.onPost().reply(201, generateLink);
 
-          const subject = await workos.portal.generateLink({
+          const adminPortalLink = await workos.portal.generateLink({
             intent: GeneratePortalLinkIntent.DSync,
             organization: 'org_01EHQMYV6MBK39QC5PZXHY59C3',
             returnUrl: 'https://www.example.com',
           });
 
-          expect(subject.link).toEqual(
+          expect(adminPortalLink).toEqual(
             'https://id.workos.com/portal/launch?secret=secret',
           );
         });
