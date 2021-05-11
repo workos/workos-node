8e17b92a5989067fcda70a2c4bbd565d80a94283
Marshall Bowers <elliott.codes@gmail.com>
2021-05-11 15:23:18 -0400

    Rename `getProfile` to `getProfileAndToken`

--- src/sso/interfaces/index.ts
+++ src/sso/interfaces/index.ts
@@ -2,6 +2,6 @@ export { AuthorizationURLOptions } from './authorization-url-options.interface';
 export { ConnectionType } from './connection-type.enum';
 export { Connection } from './connection.interface';
 export { CreateConnectionOptions } from './create-connection-options.interface';
-export { GetProfileOptions } from './get-profile-options.interface';
+export { GetProfileAndTokenOptions } from './get-profile-and-token-options.interface';
 export { Profile } from './profile.interface';
 export { PromoteDraftConnectionOptions } from './promote-draft-connection-options.interface';
