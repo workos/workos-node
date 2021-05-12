8fcb15053c2d999aa88231168921bcb2eca67ae8
Marshall Bowers <elliott.codes@gmail.com>
2021-05-12 11:11:44 -0400

    Remove `createConnection` and `promoteDraftConnection` methods

--- src/sso/interfaces/index.ts
+++ src/sso/interfaces/index.ts
@@ -1,7 +1,5 @@
-export { AuthorizationURLOptions } from './authorization-url-options.interface';
-export { ConnectionType } from './connection-type.enum';
-export { Connection } from './connection.interface';
-export { CreateConnectionOptions } from './create-connection-options.interface';
-export { GetProfileAndTokenOptions } from './get-profile-and-token-options.interface';
-export { Profile } from './profile.interface';
-export { PromoteDraftConnectionOptions } from './promote-draft-connection-options.interface';
+export * from './authorization-url-options.interface';
+export * from './connection-type.enum';
+export * from './connection.interface';
+export * from './get-profile-and-token-options.interface';
+export * from './profile.interface';
