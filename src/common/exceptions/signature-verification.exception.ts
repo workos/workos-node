242f4de59a840ce4ba1defd96c3707573a481cdc
Logan Gingerich <logangingerich@yahoo.com>
2021-10-11 16:05:43 -0600

    Error handling update

--- src/common/exceptions/signature-verification.exception.ts
+++ src/common/exceptions/signature-verification.exception.ts
@@ -1,11 +1,7 @@
 export class SignatureVerificationException extends Error {
   readonly name: string = 'SignatureVerificationException';
-  readonly message: string = `Signature verification failed.`;
 
   constructor(message: string) {
-    super();
-    if (message) {
-      this.message = message;
-    }
+    super(message || 'Signature verification failed.');
   }
 }
