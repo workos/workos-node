8a7f9b5cef115366751a06254f2577974f68a353
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2021-11-08 09:54:38 -0700

    Add line in webhooks validation to stringify payload prior to hashing expected sig

--- src/webhooks/webhooks.ts
+++ src/webhooks/webhooks.ts
@@ -47,7 +47,8 @@ export class Webhooks {
       );
     }
 
-    const expectedSig = this.computeSignature(timestamp, payload, secret);
+    const string_payload = JSON.stringify(payload)
+    const expectedSig = this.computeSignature(timestamp, string_payload, secret);
     if (this.secureCompare(expectedSig, signatureHash) === false) {
       throw new SignatureVerificationException(
         'Signature hash does not match the expected signature hash for payload',
