1b0e02fa4ce6fc24141771b174dcac0bbc792e67
Luke Morales <lukemorales@live.com>
2022-05-19 18:33:49 -0300

    Update webhook directory sync events types

--- src/webhooks/interfaces/webhook-directory.interface.ts
+++ src/webhooks/interfaces/webhook-directory.interface.ts
@@ -1,10 +1,25 @@
-import { Directory } from '../../directory-sync/interfaces';
+export type WebhookDirectoryState =
+  | 'active'
+  | 'validating'
+  | 'invalid_credentials'
+  | 'inactive'
+  | 'deleting';
 
-export interface WebhookDirectory extends Omit<Directory, 'state'> {
-  state:
-    | 'active'
-    | 'validating'
-    | 'invalid_credentials'
-    | 'inactive'
-    | 'deleting';
+interface WebhookDirectoryDomain {
+  object: 'organization_domain';
+  id: string;
+  domain: string;
+}
+
+export interface WebhookDirectory {
+  object: 'directory';
+  id: string;
+  external_key: string;
+  type: string;
+  state: WebhookDirectoryState;
+  name: string;
+  organization_id?: string;
+  domains: WebhookDirectoryDomain[];
+  created_at: string;
+  updated_at: string;
 }
