00d449143360c86ff2aa2dec7f390ec1b521ea69
Luke Morales <lukemorales@live.com>
2022-05-19 18:33:49 -0300

    Update webhook directory sync events types

--- src/webhooks/interfaces/index.ts
+++ src/webhooks/interfaces/index.ts
@@ -1,2 +1,4 @@
 export * from './webhook.interface';
 export * from './webhook-directory.interface';
+export * from './webhook-directory-group.interface';
+export * from './webhook-directory-user.interface';
