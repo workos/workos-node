d855462e1919d64a146ec95c7611c15a69ad0353
Luke Morales <lukemorales@live.com>
2022-05-19 19:39:06 -0300

    Add type for WebhookDirectoryType

--- src/webhooks/interfaces/webhook-directory.interface.ts
+++ src/webhooks/interfaces/webhook-directory.interface.ts
@@ -1,3 +1,24 @@
+export type WebhookDirectoryType =
+  | 'okta scim v1.1'
+  | 'okta scim v2.0'
+  | 'azure scim v2.0'
+  | 'bamboohr'
+  | 'breathe hr'
+  | 'cyberark scim v2.0'
+  | 'fourth hr'
+  | 'gsuite directory'
+  | 'generic scim v1.1'
+  | 'generic scim v2.0'
+  | 'gusto'
+  | 'hibob'
+  | 'jump cloud scim v2.0'
+  | 'onelogin scim v2.0'
+  | 'people hr'
+  | 'pingfederate scim v2.0'
+  | 'rippling'
+  | 's3'
+  | 'workday';
+
 export type WebhookDirectoryState =
   | 'active'
   | 'validating'
@@ -15,7 +36,7 @@ export interface WebhookDirectory {
   object: 'directory';
   id: string;
   external_key: string;
-  type: string;
+  type: WebhookDirectoryType;
   state: WebhookDirectoryState;
   name: string;
   organization_id?: string;
