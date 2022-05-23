c3b8e55205a42def6cec1a1d26069818ffc354bb
Robert Frampton <rob@workos.com>
2022-05-23 17:21:22 -0400

    v2.6.1 (#616)

--- src/workos.ts
+++ src/workos.ts
@@ -22,7 +22,7 @@ import { SSO } from './sso/sso';
 import { Webhooks } from './webhooks/webhooks';
 import { Mfa } from './mfa/mfa';
 
-const VERSION = '2.6.0';
+const VERSION = '2.6.1';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
