e925440020b0498b411f20268e6b6e25ff3f1a2d
Rob Frampton <rob@workos.com>
2022-05-23 11:49:59 -0400

    Fix handling of UnprocessableEntityException responses

--- src/common/exceptions/unprocessable-entity.exception.ts
+++ src/common/exceptions/unprocessable-entity.exception.ts
@@ -5,16 +5,39 @@ import { UnprocessableEntityError } from '../interfaces';
 export class UnprocessableEntityException extends Error {
   readonly status: number = 422;
   readonly name: string = 'UnprocessableEntityException';
-  readonly message: string;
+  readonly message: string = 'Unprocessable entity';
+  readonly code?: string;
 
-  constructor(errors: UnprocessableEntityError[], readonly requestID: string) {
+  constructor(
+    {
+      code,
+      errors,
+      message,
+    }: {
+      code?: string;
+      errors?: UnprocessableEntityError[];
+      message?: string;
+    },
+    readonly requestID: string,
+  ) {
     super();
-    const requirement: string = pluralize('requirement', errors.length);
 
-    this.message = `The following ${requirement} must be met:\n`;
+    if (message) {
+      this.message = message;
+    }
 
-    for (const { code } of errors) {
-      this.message = this.message.concat(`\t${code}\n`);
+    if (code) {
+      this.code = code;
+    }
+
+    if (errors) {
+      const requirement: string = pluralize('requirement', errors.length);
+
+      this.message = `The following ${requirement} must be met:\n`;
+
+      for (const { code } of errors) {
+        this.message = this.message.concat(`\t${code}\n`);
+      }
     }
   }
 }
