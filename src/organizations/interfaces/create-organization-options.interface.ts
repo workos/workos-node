89dafcff7a9b55de218e34e3407706e2f24c1ef5
David <86431031+dliu-workos@users.noreply.github.com>
2022-08-16 08:00:44 -0700

    Update src/organizations/interfaces/create-organization-options.interface.ts
    Co-authored-by: Marshall Bowers <elliott.codes@gmail.com>

--- src/organizations/interfaces/create-organization-options.interface.ts
+++ src/organizations/interfaces/create-organization-options.interface.ts
@@ -6,7 +6,7 @@ export interface CreateOrganizationOptions {
   domains?: string[];
 }
 
-export type CreateOrganizationRequestOptions = Pick<
+export interface CreateOrganizationRequestOptions extends Pick<
   PostOptions,
   'idempotencyKey'
 >;
