5a97eb35d2f7e38843e6a78f306e3d0c44cbc77a
Marshall Bowers <elliott.codes@gmail.com>
2022-08-18 18:31:06 -0400

    Allow customizing Axios client

--- src/common/interfaces/workos-options.interface.ts
+++ src/common/interfaces/workos-options.interface.ts
@@ -1,5 +1,8 @@
+import { AxiosRequestConfig } from 'axios';
+
 export interface WorkOSOptions {
   apiHostname?: string;
   https?: boolean;
   port?: number;
+  axios?: Omit<AxiosRequestConfig, 'baseURL'>;
 }
