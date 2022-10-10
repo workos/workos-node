b52208278c485e324c895204ca8448ea3b185465
Edita Ignot <edignot@gmail.com>
2022-10-10 10:48:12 -0600

    default value replaced with optional chaining

--- src/directory-sync/utils/get-primary-email.ts
+++ src/directory-sync/utils/get-primary-email.ts
@@ -1,6 +1,6 @@
 import { User } from '../interfaces/user.interface';
 
 export function getPrimaryEmail(user: User): string | undefined {
-  const primaryEmail = (user.emails || []).find((email) => email.primary);
+  const primaryEmail = user.emails?.find((email) => email.primary);
   return primaryEmail?.value;
 }
