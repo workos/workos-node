6512d227b6b424c31882d2f6e370625a5199cf9d
Sherry Ali <sherry@workos.com>
2023-06-01 12:04:31 -0400

    Add Events API beta

---
+++ src/events/events.ts
@@ -0,0 +1,17 @@
+import { WorkOS } from '../workos';
+import { Event } from './interfaces/event.interface';
+import { List } from '../common/interfaces/list.interface';
+import {GetEventOptions} from './interfaces';
+
+export class Events{
+  constructor(private readonly workos: WorkOS) {}
+
+  async getEvents(
+    options: GetEventOptions 
+  ): Promise<List<Event>>{
+    const { data } = await this.workos.get(`/events`, {
+      query: options,
+    });
+    return data;
+  }
+}
