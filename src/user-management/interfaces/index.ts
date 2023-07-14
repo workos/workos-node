efc5ddac444e1d4a854c7522a313ca72d6e00afe
Cameron Matheson <cameron@workos.com>
2023-07-14 13:56:45 -0600

    Add Authenticate Managed User support

--- src/user-management/interfaces/index.ts
+++ src/user-management/interfaces/index.ts
@@ -1,3 +1,4 @@
+export * from './authenticate-managed-user-options.interface';
 export * from './authenticate-unmanaged-user-options.interface';
 export * from './authentication-response.interface';
 export * from './create-unmanaged-user-options.interface';
