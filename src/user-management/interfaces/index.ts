d84c5fc9e611d00e810606a115c1784820ef69d3
Cameron Matheson <cameron@workos.com>
2023-07-14 15:48:48 -0600

    Add UserManagmement.completePasswordReset

--- src/user-management/interfaces/index.ts
+++ src/user-management/interfaces/index.ts
@@ -2,6 +2,7 @@ export * from './authenticate-managed-user-options.interface';
 export * from './authenticate-unmanaged-user-options.interface';
 export * from './authentication-response.interface';
 export * from './challenge-response.interface';
+export * from './complete-password-reset-options.interface';
 export * from './create-email-verification-challenge-options.interface';
 export * from './create-password-reset-challenge-options.interface';
 export * from './create-unmanaged-user-options.interface';
