b2a8a35d119575b6d4c608cbcce9553f63df680d
Cameron Matheson <cameron@workos.com>
2023-07-14 16:06:04 -0600

    Add UserManagement.removeUserFromOrganization

--- src/user-management/interfaces/index.ts
+++ src/user-management/interfaces/index.ts
@@ -8,6 +8,7 @@ export * from './create-email-verification-challenge-options.interface';
 export * from './create-password-reset-challenge-options.interface';
 export * from './create-unmanaged-user-options.interface';
 export * from './list-users-options.interface';
+export * from './remove-user-from-organization-options.interface';
 export * from './revoke-session-options.interface';
 export * from './user.interface';
 export * from './verify-session.interface';
