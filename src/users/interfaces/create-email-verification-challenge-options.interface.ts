151c6ffd92f8355b5ff53558bf883e2a9f355301
Cameron Matheson <cameron@workos.com>
2023-07-18 15:58:26 -0600

    Separate ChallengeResponse type

--- src/users/interfaces/create-email-verification-challenge-options.interface.ts
+++ src/users/interfaces/create-email-verification-challenge-options.interface.ts
@@ -1,4 +1,11 @@
+import { User } from './user.interface';
+
 export interface CreateEmailVerificationChallengeOptions {
   id: string;
   verification_url: string;
+}
+
+export interface CreateEmailVerificationChallengeResponse {
+  token: string;
+  user: User;
 }
