2399de5639d5f9e1df8922e7323ffcb1bf8ae42d
Cameron Matheson <cameron@workos.com>
2023-07-18 15:58:26 -0600

    Separate ChallengeResponse type

--- src/users/interfaces/create-password-reset-challenge-options.interface.ts
+++ src/users/interfaces/create-password-reset-challenge-options.interface.ts
@@ -1,4 +1,11 @@
+import { User } from './user.interface';
+
 export interface CreatePasswordResetChallengeOptions {
   email: string;
   password_reset_url: string;
+}
+
+export interface CreatePasswordResetChallengeResponse {
+  token: string;
+  user: User;
 }
