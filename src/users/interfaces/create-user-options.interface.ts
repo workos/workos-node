f1a6455d5702eab6b5175e1f7c2fe5535a9c95f9
Cameron Matheson <cameron@workos.com>
2023-07-19 10:38:50 -0600

    Rename createUnmanagedUser to createUser

--- src/users/interfaces/create-user-options.interface.ts
+++ src/users/interfaces/create-user-options.interface.ts
@@ -1,4 +1,4 @@
-export interface CreateUnmanagedUserOptions {
+export interface CreateUserOptions {
   email: string;
   password: string;
   first_name?: string;
--- src/users/interfaces/index.ts
+++ src/users/interfaces/index.ts
@@ -5,7 +5,7 @@ export * from './authentication-response.interface';
 export * from './complete-password-reset-options.interface';
 export * from './create-email-verification-challenge-options.interface';
 export * from './create-password-reset-challenge-options.interface';
-export * from './create-unmanaged-user-options.interface';
+export * from './create-user-options.interface';
 export * from './list-users-options.interface';
 export * from './remove-user-from-organization-options.interface';
 export * from './revoke-session-options.interface';
