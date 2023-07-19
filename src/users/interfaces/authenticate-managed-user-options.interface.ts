55aa200d37a9e8160b30d5da115289795ce9a29c
Cameron Matheson <cameron@workos.com>
2023-07-19 10:53:58 -0600

    authenticateManagedUser -> authenticateUserWithToken

--- src/users/interfaces/authenticate-managed-user-options.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface AuthenticateManagedUserOptions {
-  client_id: string;
-  client_secret: string;
-  grant_type: 'authorization_code';
-  code: string;
-  start_session?: boolean;
-  expires_in?: number;
-}
--- src/users/users.ts
+++ src/users/users.ts
@@ -1,8 +1,8 @@
 import { WorkOS } from '../workos';
 import {
   AddUserToOrganizationOptions,
-  AuthenticateManagedUserOptions,
   AuthenticateUserWithPasswordOptions,
+  AuthenticateUserWithTokenOptions,
   AuthenticationResponse,
   CompletePasswordResetOptions,
   CreateEmailVerificationChallengeOptions,
@@ -49,8 +49,8 @@ export class Users {
     return data;
   }
 
-  async authenticateManagedUser(
-    payload: AuthenticateManagedUserOptions,
+  async authenticateUserWithToken(
+    payload: AuthenticateUserWithTokenOptions,
   ): Promise<AuthenticationResponse> {
     const { data } = await this.workos.post('/users/sessions/token', payload);
     return data;
