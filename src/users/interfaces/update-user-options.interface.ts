6ffb19c907c7b1d3d327f06f24c16d7f05dd5893
Dane Williams <dane@workos.com>
2023-08-07 16:52:54 -0700

    Add update user method (#795)
    ## Description
    
    Adds an `updateUser` method for user management
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

---
+++ src/users/interfaces/update-user-options.interface.ts
@@ -0,0 +1,10 @@
+export interface UpdateUserOptions {
+  userId: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface SerializedUpdateUserOptions {
+  first_name: string;
+  last_name: string;
+}
