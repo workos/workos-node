a62f145efcadf062345393f5dfbc5c8a896c2113
Dane Williams <dane@workos.com>
2023-08-07 16:06:41 -0700

    delete unused import

--- src/users/users.ts
+++ src/users/users.ts
@@ -24,7 +24,6 @@ import {
   SerializedCreatePasswordResetChallengeOptions,
   SerializedCreateUserOptions,
   SerializedRevokeSessionOptions,
-  SerializedUpdateUserPasswordOptions,
   SerializedVerifySessionOptions,
   UpdateUserPasswordOptions,
   User,
