bdd62078d35b1bfced0ad05e49e45f45f7f4f9e6
Dane Williams <dane@workos.com>
2023-08-08 19:46:46 -0400

    throw if empty

--- src/users/serializers/update-user-options.serializer.ts
+++ src/users/serializers/update-user-options.serializer.ts
@@ -2,8 +2,13 @@ import { SerializedUpdateUserOptions, UpdateUserOptions } from '../interfaces';
 
 export const serializeUpdateUserOptions = (
   options: UpdateUserOptions,
-): SerializedUpdateUserOptions =>
-  ({
+): SerializedUpdateUserOptions => {
+  if (Object.keys(options).length === 0) {
+    throw new Error('At least one property must be provided');
+  }
+
+  return {
     first_name: options.firstName,
     last_name: options.lastName,
-  } as SerializedUpdateUserOptions);
+  } as SerializedUpdateUserOptions;
+};
