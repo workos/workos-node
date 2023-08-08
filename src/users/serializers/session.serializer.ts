2c7c549899013f23adeeeb9e56b624f438867c11
Oliver Zheng <services+github@oliverzheng.com>
2023-08-08 16:28:04 -0700

    Update Session interfaces to match API (#802)
    ## Description
    
    workos/workos#22251 updated the shape of the session response. This
    updates the SDK to match it.
    
    I'll update the docs as well to have the same shape as in this PR.
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [x] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/users/serializers/session.serializer.ts
+++ src/users/serializers/session.serializer.ts
@@ -1,8 +1,36 @@
-import { Session, SessionResponse } from '../interfaces/session.interface';
+import {
+  Session,
+  SessionResponse,
+  UnauthorizedOrganization,
+  UnauthorizedOrganizationReason,
+  UnauthorizedOrganizationReasonResponse,
+  UnauthorizedOrganizationResponse,
+} from '../interfaces/session.interface';
 
 export const deserializeSession = (session: SessionResponse): Session => ({
   id: session.id,
   createdAt: session.created_at,
   expiresAt: session.expires_at,
+  authorizedOrganizations: session.authorized_organizations,
+  unauthorizedOrganizations: session.unauthorized_organizations.map(
+    deserializeUnauthorizedOrganization,
+  ),
   token: session.token,
+});
+
+export const deserializeUnauthorizedOrganization = (
+  unauthorizedOrganization: UnauthorizedOrganizationResponse,
+): UnauthorizedOrganization => ({
+  organization: unauthorizedOrganization.organization,
+  reasons: unauthorizedOrganization.reasons.map(
+    deserializeUnauthorizedOrganizationReason,
+  ),
+});
+
+export const deserializeUnauthorizedOrganizationReason = (
+  unauthorizedOrganizationReason: UnauthorizedOrganizationReasonResponse,
+): UnauthorizedOrganizationReason => ({
+  type: unauthorizedOrganizationReason.type,
+  allowedAuthenticationMethods:
+    unauthorizedOrganizationReason.allowed_authentication_methods,
 });
