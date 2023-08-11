cee82d2c8e76fefedf6a2884cd6d2a8c783704fb
Ash Godfrey <ashgodfrey@Ashs-MacBook-Pro.local>
2023-08-11 15:20:05 -0600

    fix styling

--- src/common/utils/fetch-and-deserialize.ts
+++ src/common/utils/fetch-and-deserialize.ts
@@ -2,24 +2,22 @@ import { WorkOS } from '../../workos';
 import { List, ListResponse, PaginationOptions } from '../interfaces';
 import { deserializeList } from '../serializers';
 
-function setDefaultOptions(options?: PaginationOptions): PaginationOptions {
+const setDefaultOptions = (options?: PaginationOptions): PaginationOptions => {
   return {
     ...options,
     order: options?.order || 'desc',
   };
-}
+};
 
-export async function fetchAndDeserialize<T, U>(
+export const fetchAndDeserialize = async <T, U>(
   workos: WorkOS,
   endpoint: string,
   deserializeFn: (data: T) => U,
   options?: PaginationOptions,
-): Promise<List<U>> {
-  const finalOptions = setDefaultOptions(options);
-
+): Promise<List<U>> => {
   const { data } = await workos.get<ListResponse<T>>(endpoint, {
-    query: finalOptions,
+    query: setDefaultOptions(options),
   });
 
   return deserializeList(data, deserializeFn);
-}
+};
