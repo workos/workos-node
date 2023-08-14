a7db3ff82c6e943794191ae237fccc42d7d0eab6
Will Duffy <willman@workos.com>
2023-08-14 11:41:06 -0400

    Surface rawData on GenericServerException (#817)
    ## Description
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/common/exceptions/generic-server.exception.ts
+++ src/common/exceptions/generic-server.exception.ts
@@ -1,15 +1,18 @@
 export class GenericServerException extends Error {
   readonly name: string = 'GenericServerException';
+  readonly rawData: unknown;
   readonly message: string = 'The request could not be completed.';
 
   constructor(
     readonly status: number,
     message: string | undefined,
+    rawData: unknown,
     readonly requestID: string,
   ) {
     super();
     if (message) {
       this.message = message;
+      this.rawData = rawData;
     }
   }
 }
