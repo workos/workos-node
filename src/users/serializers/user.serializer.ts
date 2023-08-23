8564749217906aa62f14d89809536276101e390c
Will Duffy <willman@workos.com>
2023-08-23 15:25:51 -0400

    Rename emailVerifiedAt to emailVerified and remove type arg from listUsers (#834)
    ## Description
    
    * The API has changed and we now send email_verified instead of
    email_verified_at in our User response. This is also now a boolean
    instead of a timestamp.
    * The type was previously removed from Users and this change removes the
    now defunct type argument from listUsers.
    * Fixes USRLD-900
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/users/serializers/user.serializer.ts
+++ src/users/serializers/user.serializer.ts
@@ -4,7 +4,7 @@ export const deserializeUser = (user: UserResponse): User => ({
   object: user.object,
   id: user.id,
   email: user.email,
-  emailVerifiedAt: user.email_verified_at,
+  emailVerified: user.email_verified,
   firstName: user.first_name,
   lastName: user.last_name,
   createdAt: user.created_at,
