a06f50acdfa7242a841c31124ea2cd8d1484ab05
JB Volta <jb@workos.com>
2023-10-24 15:30:57 -0500

    Add Domain Verification API

---
+++ src/organization-domains/interfaces/create-organization-domain-options.interface.ts
@@ -0,0 +1,9 @@
+export interface CreateOrganizationDomainOptions {
+  domain: string;
+  organizationId: string;
+}
+
+export interface SerializedCreateOrganizationDomainOptions {
+  domain: string;
+  organization_id: string;
+}
---
+++ src/organization-domains/interfaces/index.ts
@@ -0,0 +1,2 @@
+export * from './create-organization-domain-options.interface';
+export * from './organization-domain.interface';
