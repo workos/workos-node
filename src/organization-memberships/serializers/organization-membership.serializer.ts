38261a60f2ee68e02e3501ce971372219c040ff5
Dane Williams <dane@workos.com>
2023-11-07 12:34:25 -0500

    done

---
+++ src/organization-memberships/serializers/organization-membership.serializer.ts
@@ -0,0 +1,15 @@
+import {
+  OrganizationMembership,
+  OrganizationMembershipResponse,
+} from '../interfaces/organization-membership.interface';
+
+export const deserializeOrganizationMembership = (
+  organizationMembership: OrganizationMembershipResponse,
+): OrganizationMembership => ({
+  object: organizationMembership.object,
+  id: organizationMembership.id,
+  userId: organizationMembership.user_id,
+  organizationId: organizationMembership.organization_id,
+  createdAt: organizationMembership.created_at,
+  updatedAt: organizationMembership.updated_at,
+});
