4781eef573a40c8d0f5a2ffc911666503e382c33
Dane Williams <dane@workos.com>
2023-11-07 13:16:38 -0500

    done

---
+++ src/organization-memberships/organization-memberships.ts
@@ -0,0 +1,50 @@
+import { fetchAndDeserialize } from '../common/utils/fetch-and-deserialize';
+import { AutoPaginatable } from '../common/utils/pagination';
+import { WorkOS } from '../workos';
+import { ListOrganizationMembershipsOptions } from './interfaces/list-organization-memberships-options.interface';
+import {
+  OrganizationMembership,
+  OrganizationMembershipResponse,
+} from './interfaces/organization-membership.interface';
+import { deserializeOrganizationMembership } from './serializers/organization-membership.serializer';
+
+export class OrganizationMemberships {
+  constructor(private readonly workos: WorkOS) {}
+
+  async getOrganizationMembership(
+    organizationMembershipId: string,
+  ): Promise<OrganizationMembership> {
+    const { data } = await this.workos.get<OrganizationMembershipResponse>(
+      `/organization_memberships/${organizationMembershipId}`,
+    );
+
+    return deserializeOrganizationMembership(data);
+  }
+
+  async listOrganizationMemberships(
+    options: ListOrganizationMembershipsOptions,
+  ): Promise<AutoPaginatable<OrganizationMembership>> {
+    return new AutoPaginatable(
+      await fetchAndDeserialize<
+        OrganizationMembershipResponse,
+        OrganizationMembership
+      >(
+        this.workos,
+        '/organization_memberships',
+        deserializeOrganizationMembership,
+        options,
+      ),
+      (params) =>
+        fetchAndDeserialize<
+          OrganizationMembershipResponse,
+          OrganizationMembership
+        >(
+          this.workos,
+          '/organization_memberships',
+          deserializeOrganizationMembership,
+          params,
+        ),
+      options,
+    );
+  }
+}
