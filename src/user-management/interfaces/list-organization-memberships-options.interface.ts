85d59b96d0445af895d1a6a0a2d3a6a0bb256d59
Dane Williams <dane@workos.com>
2023-11-08 11:14:34 -0500

    use id for name

--- src/user-management/interfaces/list-organization-memberships-options.interface.ts
+++ src/user-management/interfaces/list-organization-memberships-options.interface.ts
@@ -1,6 +1,6 @@
 import { PaginationOptions } from '../../common/interfaces';
 
 export interface ListOrganizationMembershipsOptions extends PaginationOptions {
-  organization?: string;
-  user?: string;
+  organizationId?: string;
+  userId?: string;
 }
