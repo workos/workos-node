3a55612c871c25077034c9310ca226f0f5e3e53d
Dane Williams <dane@workos.com>
2023-11-08 11:12:53 -0500

    fix file name

--- src/workos.ts
+++ src/workos.ts
@@ -27,7 +27,7 @@ import { Mfa } from './mfa/mfa';
 import { AuditLogs } from './audit-logs/audit-logs';
 import { UserManagement } from './user-management/user-management';
 import { BadRequestException } from './common/exceptions/bad-request.exception';
-import { OrganizationMemberships } from './organization-memberships/organization-membership';
+import { OrganizationMemberships } from './organization-memberships/organization-memberships';
 
 const VERSION = '3.9.0';
 
