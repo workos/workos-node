627f1b3ab575fe0aedc8d724c64f3da9c9001248
Dane Williams <dane@workos.com>
2023-11-10 22:15:32 -0500

    delete

--- src/user-management/user-management.ts
+++ src/user-management/user-management.ts
@@ -393,4 +393,12 @@ export class UserManagement {
 
     return deserializeOrganizationMembership(data);
   }
+
+  async deleteOrganizationMembership(
+    organizationMembershipId: string,
+  ): Promise<void> {
+    await this.workos.delete(
+      `/organization_memberships/${organizationMembershipId}`,
+    );
+  }
 }
