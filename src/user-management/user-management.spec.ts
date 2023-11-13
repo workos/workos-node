2dcadb7a4b069fbbae68a85eb2d3638c5a222d5c
Dane Williams <dane@workos.com>
2023-11-13 12:21:58 -0500

    adds namespace

--- src/user-management/user-management.spec.ts
+++ src/user-management/user-management.spec.ts
@@ -645,7 +645,9 @@ describe('UserManagement', () => {
   describe('deleteOrganizationMembership', () => {
     it('sends a deleteOrganizationMembership request', async () => {
       mock
-        .onDelete(`/organization_memberships/${organizationMembershipId}`)
+        .onDelete(
+          `/user_management/organization_memberships/${organizationMembershipId}`,
+        )
         .reply(200);
 
       const resp = await workos.userManagement.deleteOrganizationMembership(
@@ -653,7 +655,7 @@ describe('UserManagement', () => {
       );
 
       expect(mock.history.delete[0].url).toEqual(
-        `/organization_memberships/${organizationMembershipId}`,
+        `/user_management/organization_memberships/${organizationMembershipId}`,
       );
       expect(resp).toBeUndefined();
     });
