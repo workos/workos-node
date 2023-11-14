adcbc9a6d3ad48cc74bb1998d0fc316a015c1a10
Dane Williams <dane@workos.com>
2023-11-14 12:36:00 -0500

    fix

--- src/user-management/interfaces/create-organization-membership-options.interface.ts
+++ src/user-management/interfaces/create-organization-membership-options.interface.ts
@@ -2,3 +2,8 @@ export interface CreateOrganizationMembershipOptions {
   organizationId: string;
   userId: string;
 }
+
+export interface SerializedCreateOrganizationMembershipOptions {
+  organization_id: string;
+  user_id: string;
+}
