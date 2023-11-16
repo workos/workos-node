e0309bf26725eca4c1c4c1dc8c8fd3d823af1e02
Dane Williams <dane@workos.com>
2023-11-16 17:08:48 -0500

    adds send invitation

---
+++ src/user-management/serializers/send-invitation-options.serializer.ts
@@ -0,0 +1,14 @@
+import {} from '../interfaces';
+import {
+  SendInvitationOptions,
+  SerializedSendInvitationOptions,
+} from '../interfaces/send-invitation-options.interface';
+
+export const serializeSendInvitationOptions = (
+  options: SendInvitationOptions,
+): SerializedSendInvitationOptions => ({
+  email: options.email,
+  organization_id: options.organizationId,
+  expires_in_days: options.expiresInDays,
+  inviter_user_id: options.inviterUserId,
+});
