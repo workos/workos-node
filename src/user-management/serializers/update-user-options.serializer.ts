93d4d1e02dcb84e54c9add422d5ebd61bc8cdc20
Dane Williams <dane@workos.com>
2023-11-22 16:15:46 -0500

    updates options

--- src/user-management/serializers/update-user-options.serializer.ts
+++ src/user-management/serializers/update-user-options.serializer.ts
@@ -6,4 +6,7 @@ export const serializeUpdateUserOptions = (
   first_name: options.firstName,
   last_name: options.lastName,
   email_verified: options.emailVerified,
+  password: options.password,
+  password_hash: options.passwordHash,
+  password_hash_type: options.passwordHashType,
 });
