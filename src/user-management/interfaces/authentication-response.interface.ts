470625cc56720b5848f39e391c2dad01b4a00da0
Dane Williams <dane@workos.com>
2023-11-21 20:20:58 -0500

    Add `organizationId` to `AuthenticationResponse` (#912)
    ## Description
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.
    
    
    
    #### PR Dependency Tree
    
    
    * **PR #912** 👈
    
    This tree was auto-generated by
    [Charcoal](https://github.com/danerwilliams/charcoal)

--- src/user-management/interfaces/authentication-response.interface.ts
+++ src/user-management/interfaces/authentication-response.interface.ts
@@ -2,8 +2,10 @@ import { User, UserResponse } from './user.interface';
 
 export interface AuthenticationResponse {
   user: User;
+  organizationId: string;
 }
 
 export interface AuthenticationResponseResponse {
   user: UserResponse;
+  organization_id: string;
 }
