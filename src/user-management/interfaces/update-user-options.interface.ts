27d4eeb2c12f2bf2e1f65385234ec25f52ac2690
Dane Williams <dane@workos.com>
2023-11-22 16:15:46 -0500

    updates options

--- src/user-management/interfaces/update-user-options.interface.ts
+++ src/user-management/interfaces/update-user-options.interface.ts
@@ -3,10 +3,16 @@ export interface UpdateUserOptions {
   firstName?: string;
   lastName?: string;
   emailVerified?: boolean;
+  password?: string;
+  passwordHash?: string;
+  passwordHashType?: 'bcrypt';
 }
 
 export interface SerializedUpdateUserOptions {
   first_name?: string;
   last_name?: string;
   email_verified?: boolean;
+  password?: string;
+  password_hash?: string;
+  password_hash_type?: 'bcrypt';
 }
