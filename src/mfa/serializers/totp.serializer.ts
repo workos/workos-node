07591489782aa32969d8d8382137f6e3c3ca5c49
Dane Williams <dane@workos.com>
2023-11-27 13:24:18 -0500

    simplify totp deserializer logic

--- src/mfa/serializers/totp.serializer.ts
+++ src/mfa/serializers/totp.serializer.ts
@@ -1,18 +1,11 @@
 import { Totp, TotpResponse } from '../interfaces';
 
 export const deserializeTotp = (totp: TotpResponse): Totp => {
-  if ('qr_code' in totp && 'secret' in totp && 'uri' in totp) {
-    return {
-      issuer: totp.issuer,
-      user: totp.user,
-      qrCode: totp.qr_code,
-      secret: totp.secret,
-      uri: totp.uri,
-    };
-  } else {
-    return {
-      issuer: totp.issuer,
-      user: totp.user,
-    };
-  }
+  return {
+    issuer: totp.issuer,
+    user: totp.user,
+    qrCode: totp.qr_code,
+    secret: totp.secret,
+    uri: totp.uri,
+  };
 };
