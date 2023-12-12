8ca43dd25c1c1fd7e0a9bec4bee36db0ec0d49d7
Michael Hadley <m@mthadley.com>
2023-12-12 15:00:21 -0800

    `organizationId` should be optional in authentication responses

--- src/user-management/interfaces/authentication-response.interface.ts
+++ src/user-management/interfaces/authentication-response.interface.ts
@@ -2,10 +2,10 @@ import { User, UserResponse } from './user.interface';
 
 export interface AuthenticationResponse {
   user: User;
-  organizationId: string;
+  organizationId?: string;
 }
 
 export interface AuthenticationResponseResponse {
   user: UserResponse;
-  organization_id: string;
+  organization_id?: string;
 }
