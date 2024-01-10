b970c68cde7a0abdb72971caa89898e5b7f44c0b
Luke Morales <lukemorales@live.com>
2024-01-10 16:59:05 -0300

    Bump version

--- src/workos.ts
+++ src/workos.ts
@@ -27,7 +27,7 @@ import { AuditLogs } from './audit-logs/audit-logs';
 import { UserManagement } from './user-management/user-management';
 import { BadRequestException } from './common/exceptions/bad-request.exception';
 
-const VERSION = '5.0.0';
+const VERSION = '5.0.1';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
