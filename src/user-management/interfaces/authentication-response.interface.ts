9d756438a81e57617bbb2eedca18dc4dc393c14d
Cameron Matheson <cameron@workos.com>
2024-02-27 15:47:10 -0700

    Add session-management endpoints

--- src/user-management/interfaces/authentication-response.interface.ts
+++ src/user-management/interfaces/authentication-response.interface.ts
@@ -3,9 +3,23 @@ import { User, UserResponse } from './user.interface';
 export interface AuthenticationResponse {
   user: User;
   organizationId?: string;
+  accessToken?: string;
+  refreshToken?: string;
 }
 
 export interface AuthenticationResponseResponse {
   user: UserResponse;
   organization_id?: string;
+  access_token?: string;
+  refresh_token?: string;
+}
+
+export interface RefreshAuthenticationResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface RefreshAuthenticationResponseResponse {
+  access_token: string;
+  refresh_token: string;
 }
