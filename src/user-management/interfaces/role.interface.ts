68e2a6a20f7e9bc7d99c0e7a42d1bf27d1087047
Ameesha Isaac <ameesha@workos.com>
2024-03-25 16:56:11 -0400

    add roles events

--- src/user-management/interfaces/role.interface.ts
+++ src/user-management/interfaces/role.interface.ts
@@ -1,3 +1,18 @@
 export interface RoleResponse {
   slug: string;
 }
+
+export interface RoleEvent {
+  object: 'role';
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface RoleEventResponse {
+  object: 'role';
+  slug: string;
+  created_at: string;
+  updated_at: string;
+}
+
