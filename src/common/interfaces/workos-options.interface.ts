8ef6042b7c24ef190e992580fc8ed6ea7b590b43
Paul Asjes <paul.asjes@workos.com>
2024-04-03 11:24:41 +0200

    Added appInfo to constructor options

--- src/common/interfaces/workos-options.interface.ts
+++ src/common/interfaces/workos-options.interface.ts
@@ -1,6 +1,9 @@
+import { AppInfo } from './app-info.interface';
+
 export interface WorkOSOptions {
   apiHostname?: string;
   https?: boolean;
   port?: number;
   config?: RequestInit;
+  appInfo?: AppInfo;
 }
