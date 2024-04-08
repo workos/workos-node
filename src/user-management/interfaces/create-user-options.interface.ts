283c52872fa4533b996490277514f6eca3f126cc
amadeo <165715960+amadeo-workos@users.noreply.github.com>
2024-04-08 09:37:36 -0400

    Add 'password_hash' and 'password_hash_type' to User creation (#1014)
    ## Description
    This PR ensures that the User create process allows for the
    `password_hash` and `password_hash_type` fields, in addition to the
    existing `password` field, similar to the Update process.
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [x] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.
    
    https://github.com/workos/workos/pull/26060

--- src/user-management/interfaces/create-user-options.interface.ts
+++ src/user-management/interfaces/create-user-options.interface.ts
@@ -1,6 +1,8 @@
 export interface CreateUserOptions {
   email: string;
   password?: string;
+  passwordHash?: string;
+  passwordHashType?: 'bcrypt' | 'firebase-scrypt' | 'ssha';
   firstName?: string;
   lastName?: string;
   emailVerified?: boolean;
@@ -9,6 +11,8 @@ export interface CreateUserOptions {
 export interface SerializedCreateUserOptions {
   email: string;
   password?: string;
+  password_hash?: string;
+  password_hash_type?: 'bcrypt' | 'firebase-scrypt' | 'ssha';
   first_name?: string;
   last_name?: string;
   email_verified?: boolean;
