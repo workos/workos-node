a504b9937c8b6caf7da80e5c632fb45663ae9100
Jason Roelofs <jason.roelofs@workos.com>
2024-04-09 09:35:33 -0400

    Is setup getting invoked?

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,6 +1,8 @@
 import { enableFetchMocks } from 'jest-fetch-mock';
 import { Crypto } from '@peculiar/webcrypto';
 
+console.log("!!!!!!!\n\n\n\nEnabling fetch mocks!\n\n\n\n!!!!!!!");
+
 enableFetchMocks();
 
 // Assign Node's Crypto to global.crypto if it is not already present
