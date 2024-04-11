ccd775eed21c2dea7ca6cbe928617bf03d8aae77
Maxwell Hammad <maxwell@workos.com>
2024-04-11 18:57:29 -0400

    feat: organization events

--- src/organizations/interfaces/organization-domain.interface.ts
+++ src/organizations/interfaces/organization-domain.interface.ts
@@ -2,4 +2,7 @@ export interface OrganizationDomain {
   object: 'organization_domain';
   id: string;
   domain: string;
+  verification_token?: string;
+  verification_stragegy?: 'dns' | 'manual';
+  state?: 'failed' | 'pending' | 'legacy_verified' | 'verified';
 }
