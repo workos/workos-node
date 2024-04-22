5dbaaec756f3501c3e7bb08385e67cfb4839ffeb
Amy Hanlon <amy@workos.com>
2024-04-22 15:36:32 -0700

    remove new fields, these aren't deserialized properly

--- src/organizations/interfaces/organization-domain.interface.ts
+++ src/organizations/interfaces/organization-domain.interface.ts
@@ -2,7 +2,4 @@ export interface OrganizationDomain {
   object: 'organization_domain';
   id: string;
   domain: string;
-  verification_token?: string;
-  verification_strategy?: string;
-  state?: string;
 }
