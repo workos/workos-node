718fc427aca7bc6ca96b37cb711953b7081b85b4
Paul Asjes <p.asjes@gmail.com>
2024-06-21 14:54:16 +0200

    v7.11.1 (#1066)
    ## Description
    Bumps to v7.11.1
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/workos.ts
+++ src/workos.ts
@@ -31,7 +31,7 @@ import { HttpClient, HttpClientError } from './common/net/http-client';
 import { SubtleCryptoProvider } from './common/crypto/subtle-crypto-provider';
 import { FetchHttpClient } from './common/net/fetch-client';
 
-const VERSION = '7.11.0';
+const VERSION = '7.11.1';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
