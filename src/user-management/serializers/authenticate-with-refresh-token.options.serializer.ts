51ea13fd0769e75fc064fd06c2e64039f60ccf68
Michael Hadley <m@mthadley.com>
2024-07-29 10:18:46 -0700

    Add `organizationId` parameter to `AuthenticateWithRefreshTokenOptions`

--- src/user-management/serializers/authenticate-with-refresh-token.options.serializer.ts
+++ src/user-management/serializers/authenticate-with-refresh-token.options.serializer.ts
@@ -12,6 +12,7 @@ export const serializeAuthenticateWithRefreshTokenOptions = (
   client_id: options.clientId,
   client_secret: options.clientSecret,
   refresh_token: options.refreshToken,
+  organization_id: options.organizationId,
   ip_address: options.ipAddress,
   user_agent: options.userAgent,
 });
