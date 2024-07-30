ebe8a10cac9126d80de61206d75e40598c67a5f8
mattgd <matt.dzwonczyk@workos.com>
2024-07-30 12:05:11 -0400

    Add comment about iron-session provider.

--- src/common/iron-session/iron-session-provider.ts
+++ src/common/iron-session/iron-session-provider.ts
@@ -11,6 +11,11 @@ export type UnsealedDataType = Record<string, unknown>;
 
 /**
  * Interface encapsulating the sealData/unsealData methods for separate iron-session implementations.
+ *
+ * This allows for different implementations of the iron-session library to be used in
+ * worker/edge vs. regular web environments, which is required because of the different crypto APIs available.
+ * Once we drop support for Node 16 and upgrade to iron-session 8+, we can remove this abstraction as iron-session 8+
+ * handles this on its own.
  */
 export abstract class IronSessionProvider {
   abstract sealData(data: unknown, options: SealDataOptions): Promise<string>;
