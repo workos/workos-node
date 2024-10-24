3d882d2e3f85531a3a1e77f76b2b689593ef67e7
Amy Hanlon <amy@workos.com>
2024-10-24 10:45:45 -0700

    v7.29.1 (#1146)
    ## Description
    * #1145 
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/workos.ts
+++ src/workos.ts
@@ -33,7 +33,7 @@ import { SubtleCryptoProvider } from './common/crypto/subtle-crypto-provider';
 import { FetchHttpClient } from './common/net/fetch-client';
 import { IronSessionProvider } from './common/iron-session/iron-session-provider';
 
-const VERSION = '7.29.0';
+const VERSION = '7.29.1';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
