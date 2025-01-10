d6f179e6a2aa271155418ecdde83eaea74611fbe
Ryan Cooke <ryan.cooke@gmail.com>
2025-01-10 18:49:07 -0500

    prettier

--- src/actions/interfaces/action.interface.ts
+++ src/actions/interfaces/action.interface.ts
@@ -1,78 +1,78 @@
 import {
-    Organization,
-    OrganizationResponse,
+  Organization,
+  OrganizationResponse,
 } from '../../organizations/interfaces';
 import {
-    Invitation,
-    InvitationResponse,
-    OrganizationMembership,
-    OrganizationMembershipResponse,
-    User,
-    UserResponse,
+  Invitation,
+  InvitationResponse,
+  OrganizationMembership,
+  OrganizationMembershipResponse,
+  User,
+  UserResponse,
 } from '../../user-management/interfaces';
 
 interface AuthenticationActionContext {
-    id: string;
-    object: 'authentication_action_context';
-    user: User;
-    organization?: Organization;
-    organizationMembership?: OrganizationMembership;
-    ipAddress?: string;
-    userAgent?: string;
-    deviceFingerprint?: string;
-    issuer?: string;
+  id: string;
+  object: 'authentication_action_context';
+  user: User;
+  organization?: Organization;
+  organizationMembership?: OrganizationMembership;
+  ipAddress?: string;
+  userAgent?: string;
+  deviceFingerprint?: string;
+  issuer?: string;
 }
 
 export interface UserData {
-    object: 'user_data';
-    email: string;
-    firstName: string;
-    lastName: string;
+  object: 'user_data';
+  email: string;
+  firstName: string;
+  lastName: string;
 }
 
 interface UserRegistrationActionContext {
-    id: string;
-    object: 'user_registration_action_context';
-    userData: UserData;
-    invitation?: Invitation;
-    ipAddress?: string;
-    userAgent?: string;
-    deviceFingerprint?: string;
+  id: string;
+  object: 'user_registration_action_context';
+  userData: UserData;
+  invitation?: Invitation;
+  ipAddress?: string;
+  userAgent?: string;
+  deviceFingerprint?: string;
 }
 
 export type ActionContext =
-    | AuthenticationActionContext
-    | UserRegistrationActionContext;
+  | AuthenticationActionContext
+  | UserRegistrationActionContext;
 
 interface AuthenticationActionPayload {
-    id: string;
-    object: 'authentication_action_context';
-    user: UserResponse;
-    organization?: OrganizationResponse;
-    organization_membership?: OrganizationMembershipResponse;
-    ip_address?: string;
-    user_agent?: string;
-    device_fingerprint?: string;
-    issuer?: string;
+  id: string;
+  object: 'authentication_action_context';
+  user: UserResponse;
+  organization?: OrganizationResponse;
+  organization_membership?: OrganizationMembershipResponse;
+  ip_address?: string;
+  user_agent?: string;
+  device_fingerprint?: string;
+  issuer?: string;
 }
 
 export interface UserDataPayload {
-    object: 'user_data';
-    email: string;
-    first_name: string;
-    last_name: string;
+  object: 'user_data';
+  email: string;
+  first_name: string;
+  last_name: string;
 }
 
 export interface UserRegistrationActionPayload {
-    id: string;
-    object: 'user_registration_action_context';
-    user_data: UserDataPayload;
-    invitation?: InvitationResponse;
-    ip_address?: string;
-    user_agent?: string;
-    device_fingerprint?: string;
+  id: string;
+  object: 'user_registration_action_context';
+  user_data: UserDataPayload;
+  invitation?: InvitationResponse;
+  ip_address?: string;
+  user_agent?: string;
+  device_fingerprint?: string;
 }
 
 export type ActionPayload =
-    | AuthenticationActionPayload
-    | UserRegistrationActionPayload;
+  | AuthenticationActionPayload
+  | UserRegistrationActionPayload;
