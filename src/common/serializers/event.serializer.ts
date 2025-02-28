3e5904af00c255bf030020d112c199d73180e6d0
Kendall Strautman Swarthout <36613477+kendallstrautman@users.noreply.github.com>
2025-02-28 07:45:06 -0800

    Remove `dsync.deactivated` event (#1086)

--- src/common/serializers/event.serializer.ts
+++ src/common/serializers/event.serializer.ts
@@ -60,7 +60,6 @@ export const deserializeEvent = (event: EventResponse): Event => {
         data: deserializeConnection(event.data),
       };
     case 'dsync.activated':
-    case 'dsync.deactivated':
       return {
         ...eventBase,
         event: event.event,
