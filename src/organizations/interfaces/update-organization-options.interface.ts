55724d66694fea69de5c67efa64b89e6470268ba
Matt Dzwonczyk <9063128+mattgd@users.noreply.github.com>
2025-03-06 11:16:22 -0500

    Remove deprecated code for major version (#1224)

--- src/organizations/interfaces/update-organization-options.interface.ts
+++ src/organizations/interfaces/update-organization-options.interface.ts
@@ -7,15 +7,6 @@ export interface UpdateOrganizationOptions {
   stripeCustomerId?: string | null;
   externalId?: string | null;
   metadata?: Record<string, string>;
-
-  /**
-   * @deprecated If you need to allow sign-ins from any email domain, contact support@workos.com.
-   */
-  allowProfilesOutsideOrganization?: boolean;
-  /**
-   * @deprecated Use `domain_data` instead.
-   */
-  domains?: string[];
 }
 
 export interface SerializedUpdateOrganizationOptions {
@@ -24,13 +15,4 @@ export interface SerializedUpdateOrganizationOptions {
   stripe_customer_id?: string | null;
   external_id?: string | null;
   metadata?: Record<string, string>;
-
-  /**
-   * @deprecated If you need to allow sign-ins from any email domain, contact support@workos.com.
-   */
-  allow_profiles_outside_organization?: boolean;
-  /**
-   * @deprecated Use `domain_data` instead.
-   */
-  domains?: string[];
 }
