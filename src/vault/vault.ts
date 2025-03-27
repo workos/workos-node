6f7b5c5bd4ba4fa09d28947eae570c629ebc246b
William <william@workos.com>
2025-03-27 14:20:48 -0400

    Remove deprecated Vault Secret references (#1249)
    ## Description
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/vault/vault.ts
+++ src/vault/vault.ts
@@ -135,33 +135,4 @@ export class Vault {
     const dataKey = await this.decryptDataKey({ keys: decoded.keys });
     return decrypt(decoded, dataKey.key, associatedData || '');
   }
-
-  /*
-   * @deprecated Use `createObject` instead.
-   */
-  createSecret = this.createObject;
-  /*
-   * @deprecated Use `listObjects` instead.
-   */
-  listSecrets = this.listObjects;
-  /*
-   * @deprecated Use `listObjectVersions` instead.
-   */
-  listSecretVersions = this.listObjectVersions;
-  /*
-   * @deprecated Use `readObject` instead.
-   */
-  readSecret = this.readObject;
-  /*
-   * @deprecated Use `describeObject` instead.
-   */
-  describeSecret = this.describeObject;
-  /*
-   * @deprecated Use `updateObject` instead.
-   */
-  updateSecret = this.updateObject;
-  /*
-   * @deprecated Use `deleteObject` instead.
-   */
-  deleteSecret = this.deleteObject;
 }
