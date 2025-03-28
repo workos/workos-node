ae093f728d63955fe26e3e4bc4a1053ff0598646
mattgd <matt.dzwonczyk@workos.com>
2025-03-28 15:59:26 -0400

    Make user_id or organization_id required when calling list org. memberships.

--- src/user-management/interfaces/list-organization-memberships-options.interface.ts
+++ src/user-management/interfaces/list-organization-memberships-options.interface.ts
@@ -1,11 +1,12 @@
 import { PaginationOptions } from '../../common/interfaces';
 import { OrganizationMembershipStatus } from './organization-membership.interface';
 
-export interface ListOrganizationMembershipsOptions extends PaginationOptions {
-  organizationId?: string;
-  userId?: string;
+export type ListOrganizationMembershipsOptions = PaginationOptions & {
   statuses?: OrganizationMembershipStatus[];
-}
+} & (
+    | { organizationId: string; userId?: string }
+    | { organizationId?: string; userId: string }
+  );
 
 export interface SerializedListOrganizationMembershipsOptions
   extends PaginationOptions {
