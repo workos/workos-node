7a64d5cf44b4feeadb92389a69843fa7be2a9c80
Nick Nisi <nick.nisi@workos.com>
2025-04-01 10:16:30 -0500

    Update to latest prettier (#1253)
    >[!note]
    >This change is against the `version-8` branch.
    
    ## Description
    
    Upgrade to Prettier v3 and commit minimal changes resulting from that.
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/directory-sync/directory-sync.spec.ts
+++ src/directory-sync/directory-sync.spec.ts
@@ -471,9 +471,8 @@ describe('DirectorySync', () => {
       it(`requests a Directory User`, async () => {
         fetchOnce(userWithRoleResponse);
 
-        const subject = await workos.directorySync.getUser(
-          'directory_user_456',
-        );
+        const subject =
+          await workos.directorySync.getUser('directory_user_456');
 
         expect(subject).toEqual(userWithRoles);
       });
