305f260132da0cd3555d7abfcf546c0759b84666
Matt Dzwonczyk <9063128+mattgd@users.noreply.github.com>
2025-04-03 08:13:11 -0400

    Add tests and typing updates for pagination utils (#1252)
    Adds tests and updates typing for pagination utils.

--- src/sso/sso.ts
+++ src/sso/sso.ts
@@ -14,6 +14,7 @@ import {
   ProfileAndTokenResponse,
   ProfileResponse,
   SSOAuthorizationURLOptions,
+  SerializedListConnectionsOptions,
 } from './interfaces';
 import {
   deserializeConnection,
@@ -42,7 +43,7 @@ export class SSO {
 
   async listConnections(
     options?: ListConnectionsOptions,
-  ): Promise<AutoPaginatable<Connection>> {
+  ): Promise<AutoPaginatable<Connection, SerializedListConnectionsOptions>> {
     return new AutoPaginatable(
       await fetchAndDeserialize<ConnectionResponse, Connection>(
         this.workos,
