9dbeb099ed089481197f316cde948a730ade80db
ajworkos <adam.jenkins@workos.com>
2025-04-15 10:23:17 -0300

    version 7.47.0 (#1260)
    ## Description
    
    Patch version bump to release
    https://github.com/workos/workos-node/pull/1259
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/workos.ts
+++ src/workos.ts
@@ -37,7 +37,7 @@ import { Actions } from './actions/actions';
 import { Vault } from './vault/vault';
 import { ConflictException } from './common/exceptions/conflict.exception';
 
-const VERSION = '7.46.0';
+const VERSION = '7.47.0';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
