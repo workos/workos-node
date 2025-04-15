c702c88f3c571abe813c3dbabb16c47df97d7cf7
ajworkos <adam.jenkins@workos.com>
2025-04-15 09:44:33 -0300

    Add organizationId to organization domain (#1259)
    ## Description
    
    Add `organizationId` to the response as indicated in
    work-os-playground-domain-verification-v4hbbh
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/organization-domains/interfaces/organization-domain.interface.ts
+++ src/organization-domains/interfaces/organization-domain.interface.ts
@@ -17,6 +17,7 @@ export interface OrganizationDomain {
   object: 'organization_domain';
   id: string;
   domain: string;
+  organizationId: string;
   state: OrganizationDomainState;
   verificationToken?: string;
   verificationStrategy: OrganizationDomainVerificationStrategy;
@@ -26,6 +27,7 @@ export interface OrganizationDomainResponse {
   object: 'organization_domain';
   id: string;
   domain: string;
+  organization_id: string;
   state: OrganizationDomainState;
   verification_token?: string;
   verification_strategy: OrganizationDomainVerificationStrategy;
