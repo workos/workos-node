b67f67caeccfe4eb5d86c0872b9f4be7d367abf5
Nick Nisi <nick.nisi@workos.com>
2025-05-27 13:25:49 -0500

    [v8] Add ESM / CJS dual build (#1287)

--- src/user-management/user-management.spec.ts
+++ src/user-management/user-management.spec.ts
@@ -872,6 +872,16 @@ describe('UserManagement', () => {
   });
 
   describe('authenticateWithSessionCookie', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+      process.env = { ...OLD_ENV };
+      delete process.env.WORKOS_COOKIE_PASSWORD;
+    });
+
+    afterEach(() => {
+      process.env = OLD_ENV;
+    });
     beforeEach(() => {
       // Mock createRemoteJWKSet
       jest
@@ -1030,6 +1040,16 @@ describe('UserManagement', () => {
   });
 
   describe('getSessionFromCookie', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+      process.env = { ...OLD_ENV };
+      delete process.env.WORKOS_COOKIE_PASSWORD;
+    });
+
+    afterEach(() => {
+      process.env = OLD_ENV;
+    });
     it('throws an error when the cookie password is undefined', async () => {
       await expect(
         workos.userManagement.getSessionFromCookie({
