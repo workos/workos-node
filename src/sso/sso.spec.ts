0f888157cd6c483e3722f7d6368474eea0ec9c3b
Nick Nisi <nick.nisi@workos.com>
2025-06-12 13:02:23 -0500

    normalize user agent version in snapshot tests

--- src/sso/sso.spec.ts
+++ src/sso/sso.spec.ts
@@ -302,7 +302,12 @@ describe('SSO', () => {
           expect(fetch.mock.calls.length).toEqual(1);
 
           expect(fetchBody()).toMatchSnapshot();
-          expect(fetchHeaders()).toMatchSnapshot();
+          const headers = fetchHeaders() as Record<string, string>;
+          const normalizedHeaders = {
+            ...headers,
+            'User-Agent': headers['User-Agent'].replace(/\(node\/v[\d.]+\)/, '(node/v18.0.0)')
+          };
+          expect(normalizedHeaders).toMatchSnapshot();
           expect(accessToken).toBe('01DMEK0J53CVMC32CK5SE0KZ8Q');
           expect(profile).toMatchSnapshot();
         });
@@ -342,7 +347,12 @@ describe('SSO', () => {
           expect(fetch.mock.calls.length).toEqual(1);
 
           expect(fetchBody()).toMatchSnapshot();
-          expect(fetchHeaders()).toMatchSnapshot();
+          const headers = fetchHeaders() as Record<string, string>;
+          const normalizedHeaders = {
+            ...headers,
+            'User-Agent': headers['User-Agent'].replace(/\(node\/v[\d.]+\)/, '(node/v18.0.0)')
+          };
+          expect(normalizedHeaders).toMatchSnapshot();
           expect(accessToken).toBe('01DMEK0J53CVMC32CK5SE0KZ8Q');
           expect(profile).toMatchSnapshot();
         });
