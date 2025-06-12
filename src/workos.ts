fdc584a6cc4989fcdd4baba7f48b12aec154ac15
Nick Nisi <nick.nisi@workos.com>
2025-06-12 17:36:48 -0500

    v8.0.0-beta.2

--- src/workos.ts
+++ src/workos.ts
@@ -37,7 +37,7 @@ import { Vault } from './vault/vault';
 import { ConflictException } from './common/exceptions/conflict.exception';
 import { CryptoProvider } from './common/crypto/crypto-provider';
 
-const VERSION = '8.0.0-beta.1';
+const VERSION = '8.0.0-beta.2';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
