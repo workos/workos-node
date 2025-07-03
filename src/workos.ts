f6b2e18bd9eff653159333d06bab677a1d827f18
Adam Jenkins <adam.jenkins@workos.com>
2025-07-03 12:23:17 -0300

    Revert "v7.6.0 (#1302)"
    This reverts commit 8ebb35cbf01722986b26f4d0a55a9714bb93aa2f.

--- src/workos.ts
+++ src/workos.ts
@@ -38,7 +38,7 @@ import { Vault } from './vault/vault';
 import { ConflictException } from './common/exceptions/conflict.exception';
 import { CryptoProvider } from './common/crypto/crypto-provider';
 
-const VERSION = '7.6.0';
+const VERSION = '7.57.0';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
