498b45920aae5681a79ba184816edafd6d9b892e
Aaron Tainter <amtainter@gmail.com>
2025-07-25 15:32:28 -0700

    Bump version to 7.62.1 (#1321)

--- src/workos.ts
+++ src/workos.ts
@@ -40,7 +40,7 @@ import { ConflictException } from './common/exceptions/conflict.exception';
 import { CryptoProvider } from './common/crypto/crypto-provider';
 import { ParseError } from './common/exceptions/parse-error';
 
-const VERSION = '7.62.0';
+const VERSION = '7.62.1';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
