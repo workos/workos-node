0908443ac00d895486f4f46543f455f8797f1bcc
Stanley Phu <stanley@workos.com>
2025-08-01 13:55:32 -0700

    Add GET /organizations/:orgId/feature-flags support

---
+++ src/organizations/interfaces/feature-flag.interface.ts
@@ -0,0 +1,19 @@
+export interface FeatureFlag {
+  object: 'feature_flag';
+  id: string;
+  name: string;
+  slug: string;
+  description?: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface FeatureFlagResponse {
+  object: 'feature_flag';
+  id: string;
+  name: string;
+  slug: string;
+  description?: string;
+  created_at: string;
+  updated_at: string;
+}
