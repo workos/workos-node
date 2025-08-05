da3359941815626d075d1adeeae009c437154c51
Nick Nisi <nick.nisi@workos.com>
2025-08-05 10:55:05 -0700

    build fixes

--- src/user-management/user-management.ts
+++ src/user-management/user-management.ts
@@ -42,6 +42,7 @@ import {
   SerializedCreateMagicAuthOptions,
   SerializedCreatePasswordResetOptions,
   SerializedCreateUserOptions,
+  SerializedListSessionsOptions,
   SerializedListUsersOptions,
   SerializedResetPasswordOptions,
   SerializedVerifyEmailOptions,
@@ -709,7 +710,7 @@ export class UserManagement {
   async listSessions(
     userId: string,
     options?: ListSessionsOptions,
-  ): Promise<AutoPaginatable<Session>> {
+  ): Promise<AutoPaginatable<Session, SerializedListSessionsOptions>> {
     return new AutoPaginatable(
       await fetchAndDeserialize<SessionResponse, Session>(
         this.workos,
