f0fee7d4ceccf9e6307d922f4af1b6517c2398cc
Colin Barber <colin@workos.com>
2025-09-17 10:37:48 -0300

    Correct create org membership types

--- src/user-management/interfaces/create-organization-membership-options.interface.ts
+++ src/user-management/interfaces/create-organization-membership-options.interface.ts
@@ -2,12 +2,12 @@ export interface CreateOrganizationMembershipOptions {
   organizationId: string;
   userId: string;
   roleSlug?: string;
-  roleSlugs?: string;
+  roleSlugs?: string[];
 }
 
 export interface SerializedCreateOrganizationMembershipOptions {
   organization_id: string;
   user_id: string;
   role_slug?: string;
-  role_slugs?: string;
+  role_slugs?: string[];
 }
