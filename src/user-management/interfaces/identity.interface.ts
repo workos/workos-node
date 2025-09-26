72394a8502c4780db0843c9aaebf096f2e4621c0
antn <anton@antn.me>
2025-09-26 03:35:50 -0700

    run prettier

--- src/user-management/interfaces/identity.interface.ts
+++ src/user-management/interfaces/identity.interface.ts
@@ -1,11 +1,21 @@
 export interface Identity {
   idpId: string;
   type: 'OAuth';
-  provider: 'AppleOAuth' | 'GoogleOAuth' | 'GitHubOAuth' | 'MicrosoftOAuth' | 'SalesforceOAuth';
+  provider:
+    | 'AppleOAuth'
+    | 'GoogleOAuth'
+    | 'GitHubOAuth'
+    | 'MicrosoftOAuth'
+    | 'SalesforceOAuth';
 }
 
 export interface IdentityResponse {
   idp_id: string;
   type: 'OAuth';
-  provider: 'AppleOAuth' | 'GoogleOAuth' | 'GitHubOAuth' | 'MicrosoftOAuth' | 'SalesforceOAuth';
+  provider:
+    | 'AppleOAuth'
+    | 'GoogleOAuth'
+    | 'GitHubOAuth'
+    | 'MicrosoftOAuth'
+    | 'SalesforceOAuth';
 }
