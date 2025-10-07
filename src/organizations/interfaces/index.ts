cef662beb497c5f7962d4a9c3b97ba6d577e41c0
Stanley Phu <stanley@workos.com>
2025-10-07 09:34:11 -0700

    Refactor feature flag types to shared module

--- src/organizations/interfaces/index.ts
+++ src/organizations/interfaces/index.ts
@@ -1,6 +1,5 @@
 export * from './create-organization-options.interface';
 export * from './domain-data.interface';
-export * from './feature-flag.interface';
 export * from './list-organization-feature-flags-options.interface';
 export * from './list-organizations-options.interface';
 export * from './organization.interface';
