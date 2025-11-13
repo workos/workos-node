9d177b5a1f8ef6ddb5d125769004967700842558
Adam Wolfman <47123183+awolfden@users.noreply.github.com>
2025-11-13 08:55:55 -0700

    Add argon2 type to PasswordHashType interface (#1384)
    ## Description
    
    Add argon2 type to PasswordHashType interface
    
    ## Documentation
    
    Does this require changes to the WorkOS Docs? E.g. the [API
    Reference](https://workos.com/docs/reference) or code snippets need
    updates.
    
    ```
    [ ] Yes
    ```
    
    If yes, link a related docs PR and add a docs maintainer as a reviewer.
    Their approval is required.

--- src/user-management/interfaces/password-hash-type.interface.ts
+++ src/user-management/interfaces/password-hash-type.interface.ts
@@ -1,1 +1,6 @@
-export type PasswordHashType = 'bcrypt' | 'firebase-scrypt' | 'ssha' | 'scrypt';
+export type PasswordHashType =
+  | 'bcrypt'
+  | 'firebase-scrypt'
+  | 'ssha'
+  | 'scrypt'
+  | 'argon2';
