2fbf3db3aa28f0e8363722b5ab3d465e70e02d56
Nick Nisi <nick.nisi@workos.com>
2025-12-04 15:55:29 -0600

    simplify build file, add ATTW CLI to check types

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,55 +1,24 @@
 import { defineConfig } from 'tsdown';
 
-const entry = [
-  'src/**/*.ts',
-  '!src/**/*.spec.ts',
-  '!src/**/*.test.ts',
-  '!src/**/fixtures/**',
-  '!src/**/*.d.ts',
-  '!src/**/test-utils.ts',
-];
-
-export default defineConfig([
-  // ESM build
-  {
-    entry,
-    format: 'esm',
-    outDir: 'lib/esm',
-    target: 'es2022',
-    sourcemap: true,
-    clean: true,
-    dts: false,
-    unbundle: true,
-    outExtensions() {
-      return { js: '.js' };
-    },
+export default defineConfig({
+  entry: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/*.test.ts',
+    '!src/**/fixtures/**',
+    '!src/**/*.d.ts',
+    '!src/**/test-utils.ts',
+  ],
+  format: ['esm', 'cjs'],
+  outDir: 'lib',
+  target: 'es2022',
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  unbundle: true,
+  outExtensions({ format }) {
+    return {
+      js: format === 'cjs' ? '.cjs' : '.js',
+    };
   },
-  // CJS build
-  {
-    entry,
-    format: 'cjs',
-    outDir: 'lib/cjs',
-    target: 'es2022',
-    sourcemap: true,
-    clean: true,
-    dts: false,
-    unbundle: true,
-    outExtensions() {
-      return { js: '.cjs' };
-    },
-  },
-  // Types only
-  {
-    entry,
-    clean: true,
-    format: 'esm',
-    outDir: 'lib/types',
-    dts: {
-      emitDtsOnly: true,
-    },
-    unbundle: true,
-    outExtensions() {
-      return { dts: '.d.ts' };
-    },
-  },
-]);
+});
