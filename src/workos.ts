14161208882b6d93cc6e98390bfee9d6acbc6217
Nick Nisi <nick.nisi@workos.com>
2026-01-08 14:04:34 -0600

    v8.0.0-rc.8

--- src/workos.ts
+++ src/workos.ts
@@ -45,7 +45,7 @@ import { ParseError } from './common/exceptions/parse-error';
 import { getEnv } from './common/utils/env';
 import { getRuntimeInfo } from './common/utils/runtime-info';
 
-const VERSION = '8.0.0-rc.7';
+const VERSION = '8.0.0-rc.8';
 
 const DEFAULT_HOSTNAME = 'api.workos.com';
 
