8b7efb2a2595c0dffc3e1b1b3f5879f60bb19e64
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2026-04-17 16:10:34 -0500

    Migrate portal to admin portal

--- src/portal/interfaces/generate-portal-link-intent.interface.ts
+++
@@ -1,9 +0,0 @@
-export enum GeneratePortalLinkIntent {
-  AuditLogs = 'audit_logs',
-  DomainVerification = 'domain_verification',
-  DSync = 'dsync',
-  LogStreams = 'log_streams',
-  SSO = 'sso',
-  CertificateRenewal = 'certificate_renewal',
-  BringYourOwnKey = 'bring_your_own_key',
-}
