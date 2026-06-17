97a7c3adab4a0607550c6b82b37a3c4ff8f51921
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2026-06-16 20:52:30 -0400

    Generate organizaions APIs using oagen

--- src/organizations/interfaces/list-organizations-options.interface.ts
+++ src/organizations/interfaces/list-organizations-options.interface.ts
@@ -3,4 +3,6 @@ import { PaginationOptions } from '../../common/interfaces/pagination-options.in
 export interface ListOrganizationsOptions extends PaginationOptions {
   /** The domains of an Organization. Any Organization with a matching domain will be returned. */
   domains?: string[];
+  /** Searchable text for an Organization. Matches against the Organization name and domains. */
+  search?: string;
 }
